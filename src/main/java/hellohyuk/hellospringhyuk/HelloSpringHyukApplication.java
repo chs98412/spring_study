95165d598128a4ebdda482f7cb70e2deef7dd382
ChoiHyukSoon <chs98412@gmail.com>
2021-05-02 22:48:29 +0900

    first

---
+++ src/main/java/hellohyuk/hellospringhyuk/HelloSpringHyukApplication.java
@@ -0,0 +1,13 @@
+package hellohyuk.hellospringhyuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringHyukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringHyukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hellohyuk/hellospringhyuk/HelloSpringHyukApplicationTests.java
@@ -0,0 +1,13 @@
+package hellohyuk.hellospringhyuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringHyukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
